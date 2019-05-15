d88751977b2d916a209e64d6b69fd68733e1edf7
Tual alexandre <tual.alexandre@gmail.com>
2019-05-15 14:26:48 +0200

    creation of the Customer entity and implementation of the JWT creation system

---
+++ src/Entity/Customer.php
@@ -0,0 +1,171 @@
+<?php
+
+namespace App\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+use JMS\Serializer\Annotation as Serializer;
+use Symfony\Bridge\Doctrine\Validator\Constraints\UniqueEntity;
+use Symfony\Component\Security\Core\User\UserInterface;
+use Symfony\Component\Validator\Constraints as Assert;
+
+/**
+ * @ORM\Entity(repositoryClass="App\Repository\CustomerRepository")
+ * @UniqueEntity("siren")
+ */
+class Customer implements UserInterface
+{
+    /**
+     * @ORM\Id()
+     * @ORM\GeneratedValue()
+     * @ORM\Column(type="integer")
+     * @Serializer\Groups({"list", "detail", "create"})
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     * @Assert\NotBlank()
+     * @Serializer\Groups({"list", "detail", "create"})
+     */
+    private $name;
+
+    /**
+     * @ORM\Column(type="string", length=255, unique=true)
+     * @Assert\Email()
+     * @Serializer\Groups({"detail", "create"})
+     */
+    private $email;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     * @Assert\Length(min="6")
+     * @Serializer\Groups("create")
+     */
+    private $hash;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     * @Assert\Regex("/^0([1-9]{1})([0-9]{9})$/, message=""number.only")
+     * @Serializer\Groups({"detail", "create"})
+     */
+    private $phone;
+
+    /**
+     * @ORM\Column(type="string", length=255, unique=true)
+     * @Assert\NotBlank()
+     * @Assert\Length(min="9", max="9")
+     * @Assert\Regex("/^[0-9]{9}$/, message=""number.only")
+     * @Serializer\Groups({"detail", "create"})
+     */
+    private $siren;
+
+    /**
+     * @ORM\Column(type="string", length=255, nullable=true)
+     * @Assert\Length(min="14", max="14")
+     * @Assert\Regex("/^[0-9]{14}$/, message=""number.only")
+     * @Serializer\Groups({"detail", "create"})
+     */
+    private $siret;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getName(): ?string
+    {
+        return $this->name;
+    }
+
+    public function setName(string $name): self
+    {
+        $this->name = $name;
+
+        return $this;
+    }
+
+    public function getEmail(): ?string
+    {
+        return $this->email;
+    }
+
+    public function setEmail(string $email): self
+    {
+        $this->email = $email;
+
+        return $this;
+    }
+
+    public function getHash(): ?string
+    {
+        return $this->hash;
+    }
+
+    public function setHash(string $hash): self
+    {
+        $this->hash = $hash;
+
+        return $this;
+    }
+
+    public function getPhoneNumber(): ?string
+    {
+        return $this->phone;
+    }
+
+    public function setPhoneNumber(string $phone): self
+    {
+        $this->phone = $phone;
+
+        return $this;
+    }
+
+    public function getSiret(): ?string
+    {
+        return $this->siret;
+    }
+
+    public function setSiret(string $siret): self
+    {
+        $this->siret = $siret;
+
+        return $this;
+    }
+
+    public function getSiren(): ?string
+    {
+        return $this->siren;
+    }
+
+    public function setSiren(string $siren): self
+    {
+        $this->siren = $siren;
+
+        return $this;
+    }
+
+    public function getRoles()
+    {
+        return ['ROLE_CUSTOMER'];
+    }
+
+    public function getPassword()
+    {
+        return $this->hash;
+    }
+
+    public function getSalt()
+    {
+        return null;
+    }
+
+    public function getUsername()
+    {
+        return $this->getEmail();
+    }
+
+    public function eraseCredentials()
+    {
+        // TODO: Implement eraseCredentials() method.
+    }
+}
