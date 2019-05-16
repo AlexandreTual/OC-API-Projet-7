3f2c69e007b26bc246c9ee23c765677aae660713
Tual alexandre <tual.alexandre@gmail.com>
2019-05-16 22:37:55 +0200

    securing user functions

--- src/Controller/UserController.php
+++ src/Controller/UserController.php
@@ -9,6 +9,8 @@ use Doctrine\Common\Persistence\ObjectManager;
 use JMS\Serializer\DeserializationContext;
 use JMS\Serializer\SerializationContext;
 use JMS\Serializer\SerializerInterface;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\IsGranted;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Security;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
@@ -25,10 +27,11 @@ class UserController extends AbstractController
 
     /**
      * @Route(path="/api/user", methods={"GET"})
+     * @IsGranted("ROLE_CUSTOMER")
      */
     public function list(UserRepository $repo, SerializerInterface $serializer)
     {
-        $users = $repo->findAll();
+        $users = $repo->findBy(['customer' => $this->customerService->getUser()]);
 
         $data = $serializer->serialize($users, 'json', SerializationContext::create()->setGroups(['list']));
 
@@ -40,13 +43,14 @@ class UserController extends AbstractController
 
     /**
      * @Route(path="/api/user/{id}", methods={"GET"})
+     * @Security("user.getId() == john.getCustomer().getId()")
      * @param User $user
      * @param SerializerInterface $serializer
      * @return Response
      */
-    public function show(User $user, SerializerInterface $serializer)
+    public function show(User $john, SerializerInterface $serializer)
     {
-        $data = $serializer->serialize($user, 'json', SerializationContext::create()->setGroups(['detail']));
+        $data = $serializer->serialize($john, 'json', SerializationContext::create()->setGroups(['detail']));
 
         $response = new Response($data, Response::HTTP_OK);
         $response->headers->set('Content-type', 'application/json');
@@ -56,6 +60,7 @@ class UserController extends AbstractController
 
     /**
      * @Route(path="/api/user/create", methods={"POST"})
+     * @IsGranted("ROLE_CUSTOMER")
      * @param Request $request
      * @param SerializerInterface $serializer
      * @param ObjectManager $manager
@@ -79,13 +84,14 @@ class UserController extends AbstractController
 
     /**
      * @Route(path="/api/user/delete/{id}", methods={"DELETE"})
+     * @Security("user.getId() == john.getCustomer().getId()")
      * @param Request $request
      * @param ObjectManager $manager
      * @param SerializerInterface $serializer
      */
-    public function delete(User $user, ObjectManager $manager)
+    public function delete(User $john, ObjectManager $manager)
     {
-        $manager->remove($user);
+        $manager->remove($john);
         $manager->flush();
 
         return new Response('', Response::HTTP_OK);
